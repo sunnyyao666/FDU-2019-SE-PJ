0fe06396ef4aa73d1878e54acc3f45a6b557740d
Wang tian <869006243@qq.com>
2020-03-24 11:55:25 +0800

    Update CorsConfig.java

--- src/main/java/fudan/se/lab2/config/CorsConfig.java
+++ src/main/java/fudan/se/lab2/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").
-                        allowedOrigins("*"). //允许跨域的域名，可以用*表示允许任何域名使用
+                        allowedOrigins("http://localhost","http://114.115.160.99"). //允许跨域的域名，可以用*表示允许任何域名使用
                         allowedMethods("*"). //允许任何方法（post、get等）
                         allowedHeaders("*"). //允许任何请求头
                         allowCredentials(true). //带上cookie信息
